4ede22b9c3ad5801683aaa4d603c07c9f5b5cf1e
NelleFavoreel <nelle.favoreel@student.ehb.be>
2024-11-30 13:34:18 +0100

    feat: added router and pages to project

---
+++ frontend/src/pages/Data.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Data() {
+	return (
+		<div>
+			<h2>Data Page</h2>
+			<p>Welcome to the home page!</p>
+		</div>
+	);
+}
+
+export default Data;
---
+++ frontend/src/pages/ShowData.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ShowData() {
+	return (
+		<div>
+			<h2>Showdata Page</h2>
+			<p>Welcome to the home page!</p>
+		</div>
+	);
+}
+
+export default ShowData;
