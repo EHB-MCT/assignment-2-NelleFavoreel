d4249ecc821d935e6832d2d2336ed85ed951f21d
NelleFavoreel <nelle.favoreel@student.ehb.be>
2024-12-01 12:10:10 +0100

    style: navigation and body

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Data from "./pages/Data";
 import ShowData from "./pages/ShowData";
-import Nav from "./components/Nav";
 
 function App() {
 	return (
