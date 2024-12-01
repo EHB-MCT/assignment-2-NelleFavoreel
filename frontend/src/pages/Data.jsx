4455f2bfb8f5172da48148de2340847f04e8c508
NelleFavoreel <nelle.favoreel@student.ehb.be>
2024-12-01 12:23:30 +0100

    feat: added logic for data tracking

--- frontend/src/pages/Data.jsx
+++ frontend/src/pages/Data.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import TrackingExample from "../components/TrackingExample";
 
 function Data() {
 	return (
 		<div>
-			<h2>Data Page</h2>
-			<p>Welcome to the home page!</p>
+			<TrackingExample></TrackingExample>
 		</div>
 	);
 }
